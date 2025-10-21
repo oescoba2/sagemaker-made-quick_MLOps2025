14fe85e03c8ca8f540da74f97a5dd241f318a813
Óscar Escobar <101133127+oescoba2@users.noreply.github.com>
2025-10-21 11:14:58 -0600

    Update S3 bucket name in backend configuration
    Added my bucket name mlopsf2025 from us-east-1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "cs401rex" # TODO: replace with your bucket name
+    bucket  = "mlopsf2025" # TODO: replace with your bucket name
     key     = "terraform/sagemaker-domain.tfstate"
     region  = "us-east-1"
     encrypt = true
